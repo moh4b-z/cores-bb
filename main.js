138e269c0a5d275fea34069f6a0fd67b38e75773
MOHAMMAD SALIM SOARES SILVA <24122313@CFP127ALUNO.LOCAL>
2025-01-30 09:52:05 -0300

    Alterações no CSS e no JS, para mundaça de cor de fundo

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 "use stict"
 
-const trocarCor = document.getElementById('trocar-cor')
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+function trocarCor(){
+    const cor = document.getElementById('cor').value
+    document.documentElement.style.setProperty('--cor-fundo', cor)
+}
+
+botaoTrocarCor.addEventListener('click', trocarCor)
